4b165d86c7e5e667b877e11a6b628d1fac2fda7a
Michael Prather <mprather1@gmail.com>
2016-04-20 21:32:10 -0500

    created rails app, going to try and connect them together

--- backbone_frontend/app/assets/javascripts/app/views/userView.js
+++ backbone_frontend/app/assets/javascripts/app/views/userView.js
@@ -3,7 +3,7 @@ var UserView = Backbone.View.extend({
   tagName: 'li',
 
   initialize: function() {
-    console.log(this.model);
+    console.log(this.model.toJSON());
   },
 
   render: function() {
