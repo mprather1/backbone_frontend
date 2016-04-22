0b6d9dfa2d30fb04c1489146e6aeedaf5b800046
Michael Prather <mprather1@gmail.com>
2016-04-21 20:27:50 -0500

    rake task created/users controller outputting JSON

--- backbone_frontend/config/routes.rb
+++ backbone_frontend/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
